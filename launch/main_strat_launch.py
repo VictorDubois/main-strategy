73fb33229ebfb092ac98cc237e3ea243b2cc0f64
Victor on ROS2 VM <victor.dubois@free.fr>
2024-04-28 19:34:50 +0200

    fix position init

--- launch/main_strat_launch.py
+++ launch/main_strat_launch.py
@@ -1,9 +1,39 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import launch_ros.actions
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 
 def generate_launch_description():
+    isBlue_value = LaunchConfiguration('isBlue')
+    xRobotPos_value = LaunchConfiguration('xRobotPos')
+    yRobotPos_value = LaunchConfiguration('yRobotPos')
+    zRobotOrientation_value = LaunchConfiguration('zRobotOrientation')
+    
+    isBlue_launch_arg = DeclareLaunchArgument(
+        'isBlue',
+        default_value='false'
+    )
+    xRobotPos_launch_arg = DeclareLaunchArgument(
+        'xRobotPos',
+        default_value='1.25'
+    )
+    yRobotPos_launch_arg = DeclareLaunchArgument(
+        'yRobotPos',
+        default_value='0.5'
+    )
+    zRobotOrientation_launch_arg = DeclareLaunchArgument(
+        'zRobotOrientation',
+        default_value='0.0'
+    )
     return LaunchDescription([
+        isBlue_launch_arg, xRobotPos_launch_arg, yRobotPos_launch_arg, zRobotOrientation_launch_arg,
+        launch_ros.actions.SetParameter(name='isBlue', value=isBlue_value),
+        launch_ros.actions.SetParameter(name='xRobotPos', value=xRobotPos_value),
+        launch_ros.actions.SetParameter(name='yRobotPos', value=yRobotPos_value),
+        launch_ros.actions.SetParameter(name='zRobotOrientation', value=zRobotOrientation_value),
         Node(
+            #prefix=['gdbserver localhost:3000'],
             package='main_strategy',
             namespace='krabi_ns',
             executable='main_strategy_node',
--- launch/odom_TF_pub_launch.py
+++ launch/odom_TF_pub_launch.py
@@ -1,8 +1,32 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+import launch_ros.actions
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 def generate_launch_description():
+    isBlue_value = LaunchConfiguration('isBlue')
+    xRobotPos_value = LaunchConfiguration('xRobotPos')
+    yRobotPos_value = LaunchConfiguration('yRobotPos')
+    zRobotOrientation_value = LaunchConfiguration('zRobotOrientation')
+    
+    isBlue_launch_arg = DeclareLaunchArgument(
+        'isBlue',
+        default_value='false'
+    )
+    xRobotPos_launch_arg = DeclareLaunchArgument(
+        'xRobotPos',
+        default_value='1.25'
+    )
+    yRobotPos_launch_arg = DeclareLaunchArgument(
+        'yRobotPos',
+        default_value='0.5'
+    )
+    zRobotOrientation_launch_arg = DeclareLaunchArgument(
+        'zRobotOrientation',
+        default_value='0.0'
+    )
     return LaunchDescription([
+        isBlue_launch_arg, xRobotPos_launch_arg, yRobotPos_launch_arg, zRobotOrientation_launch_arg,
         Node(
             package='main_strategy',
             namespace='krabi_ns',
@@ -13,9 +37,9 @@ def generate_launch_description():
             #    ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
             #]
             ,parameters=[
-            {"init_pose/x": 0.0},
-            {"init_pose/y": 0.0},
-            {"init_pose/theta": 0.0},
+            {"init_pose/x": xRobotPos_value},
+            {"init_pose/y": yRobotPos_value},
+            {"init_pose/theta": zRobotOrientation_value},
             {"publish_tf_odom": True}
         ]
         )
